c90c3d752042fd7114c606b6d9d6d54632aee9d4
Samuel <sojogbede1@gmail.com>
2020-08-30 00:57:21 +0100

    Unit test altered to check through templates instead

--- blog/tests.py
+++ blog/tests.py
@@ -1,18 +1,9 @@
-from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
-from blog.views import *
 
 class CVPageTest(TestCase):
 
-    def test_url_resolves_to_cv_page_view(self):
-        found = resolve('/cv/edit/')  
-        self.assertEqual(found.func, cv_edit)
+    def test_uses_cv_edit_template(self):
+        response = self.client.get('/cv/edit/')
+        self.assertTemplateUsed(response, 'blog/cv_edit.html')
 
-    def test_cv_page_returns_correct_html(self):
-        request = HttpRequest()  
-        response = cv_edit(request)  
-        html = response.content.decode('utf8')  
-        self.assertTrue(html.startswith('\n<html>'))  
-        self.assertIn('<title>CV page</title>', html)  
-        self.assertTrue(html.endswith('</html>'))
+  
