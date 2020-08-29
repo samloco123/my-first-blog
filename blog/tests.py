dd6952474b43f04ddf88226fa1e76f45db1f3a9d
Samuel <sojogbede1@gmail.com>
2020-08-29 22:03:09 +0100

    Basic view now returns minimal/base HTML

--- blog/tests.py
+++ blog/tests.py
@@ -1,9 +1,18 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 from blog.views import *
 
 class CVPageTest(TestCase):
 
-    def test_url_resolves_to_cve_page_view(self):
+    def test_url_resolves_to_cv_page_view(self):
         found = resolve('/cv/edit/')  
         self.assertEqual(found.func, cv_edit)
+
+    def test_cv_page_returns_correct_html(self):
+        request = HttpRequest()  
+        response = cv_edit(request)  
+        html = response.content.decode('utf8')  
+        self.assertTrue(html.startswith('\n<html>'))  
+        self.assertIn('<title>CV page</title>', html)  
+        self.assertTrue(html.endswith('</html>'))
